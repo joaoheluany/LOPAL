244d723b031b95fc344d22d07691186a3b9fba74
joaoheluany <jvhf.iza@gmail.com>
2026-03-11 16:43:08 -0300

    feat calculadora terminada e iniciada urna eletrônica

---
+++ idade/idade.js
@@ -0,0 +1,9 @@
+function idade(){
+    let idade;
+    do{
+        idade = parseInt (prompt("informe sua idade (valores aceitos de 5 a 150"));
+    } while(idade < 5 || idade > 150|| idade != idade);
+    
+    alert("idade validada")
+
+}
